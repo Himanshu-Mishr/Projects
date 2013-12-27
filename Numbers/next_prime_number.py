c94e9f7b552645d3f52963e03d003ba2002371f1
Himanshu Mishra <himanshu.m786@gmail.com>
2013-12-27 15:49:33 +0530

    documenting Number/next_prime_number.py

--- Numbers/next_prime_number.py
+++ Numbers/next_prime_number.py
@@ -3,19 +3,34 @@
 # about : Next Prime Number
 # algorithm used : calculating prime numbers
 # links : Numbers/next_prime_number.py
+# -------------------------------------------------------
 
+# 
+# global declaration of prime varible 
+# inorder to sync with both function
 prime = 0
 
+# calculates next prime 
+# Parameter : y = previous prime number
 def primeCheck(y):
+
+    # while runs till the next prime number is found
     while(True):
+        # incrementing to next number
         y += 1
+
+        # logic : number above y//2 is required for division
         x= y//2
+
         while x>1:
             if y%x == 0:
                 break
             x -= 1
+        
         else:
             print(" ⇒  Next Prime : ",y)
+            
+            # sync the global prime number
             global prime
             prime = y
             break
@@ -23,8 +38,11 @@ def primeCheck(y):
     
     
 
-# Have the program find prime numbers until the user chooses to stop asking for the next one
+# Have the program find prime numbers until 
+# the user chooses to stop asking for the next one
 def main():
+    
+    # printing information for the user
     info = """
     Program Name : Next Prime Number
 
@@ -32,15 +50,22 @@ def main():
     To Exit just press Enter 【 ↵ 】.
     """
     print(info)
+    
+    # using global 
     global prime
-    try :
+
+    # asking user from where he/she wants to starts
+    try :  
         prime  =  int(input("From where you want to start : "))
     except:
+        # default start 1 , this doesn't mean 1st prime is 1 :P
         prime = 1
 
-    #print("Next Prime : ",prime)
+    # while keeps running as user enter "next" anything other 
+    # than "next" will stop the loop and excutes the else block statement  
     while input(" ⇐ ") == "next" :
         primeCheck(prime)
+    
     else:
         print("-------------------")
         print("Exiting Program")
